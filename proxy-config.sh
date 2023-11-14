b5fd23ff86e375d2b4baeb0dfb013c3d90ab3047
Trevor Du <trevor@dropbase.io>
2023-11-14 12:09:53 -0500

    fix proxy build steps

--- proxy-config.sh
+++ proxy-config.sh
@@ -2,7 +2,7 @@
 cat << EOF > ./frpc.toml
 # frpc.toml
 serverAddr = "${DROPBASE_PROXY_SERVER_URL}"
-serverPort = ${FRPS_SERVER_PORT}
+serverPort = 7000
 
 metadatas.token = "${DROPBASE_PROXY_SERVER_TOKEN}"
 
@@ -10,12 +10,12 @@ metadatas.token = "${DROPBASE_PROXY_SERVER_TOKEN}"
 name = "${DROPBASE_PROXY_SERVER_TOKEN}/worker"
 type = "tcp"
 localIP = "127.0.0.1"
-localPort = ${DROPBASE_WORKER_PORT}
+localPort = ${DROPBASE_WORKER_PORT:-9000}
 
 [[proxies]]
 name = "${DROPBASE_PROXY_SERVER_TOKEN}/lsp"
 type = "tcp"
 localIP = "127.0.0.1"
-localPort = ${DROPBASE_LSP_PORT}
+localPort = ${DROPBASE_LSP_PORT:-9001}
 EOF
 exec /usr/bin/frpc -c ./frpc.toml
