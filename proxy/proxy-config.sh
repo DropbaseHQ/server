363b1135a68bba92235ae6ce44df0d950069bec2
Ayazhan Zhakhan <46544198+azhakhan@users.noreply.github.com>
2023-11-14 14:30:29 -0600

    Az/adding docker (#2)
    * added docker files for server and lsp
    
    * add frp source code
    
    * fix proxy build steps
    
    * fix networking between containers
    
    * clean up proxy files
    
    * remove chmod from Dockerfile-proxy
    
    ---------
    
    Co-authored-by: Trevor Du <trevor@dropbase.io>

---
+++ proxy/proxy-config.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cat << EOF > ./frpc.toml
+# frpc.toml
+serverAddr = "${DROPBASE_PROXY_SERVER_URL}"
+serverPort = 7000
+
+metadatas.token = "${DROPBASE_PROXY_SERVER_TOKEN}"
+
+[[proxies]]
+name = "${DROPBASE_PROXY_SERVER_TOKEN}/worker"
+type = "tcp"
+localIP = "dropbase-server"
+localPort = ${DROPBASE_WORKER_PORT:-9000}
+
+[[proxies]]
+name = "${DROPBASE_PROXY_SERVER_TOKEN}/lsp"
+type = "tcp"
+localIP = "dropbase-lsp"
+localPort = ${DROPBASE_LSP_PORT:-9001}
+EOF
+exec /usr/bin/frpc -c ./frpc.toml
