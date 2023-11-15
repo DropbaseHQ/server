7c4e98a76241c246fd4a5ff29d1f9c65cec32314
Jonathan <jon@dropbase.io>
2023-11-15 15:05:46 -0500

    Server requests are now in a router class

--- server/requests/main_request.py
+++ server/requests/main_request.py
@@ -15,4 +15,19 @@ class DropbaseSession(Session):
         return super().request(method, url, *args, **kwargs)
 
 
+# class DropbaseSession(Session):
+#     def __init__(self, base_url=None, access_token_header=None, refresh_token_header=None):
+#         super().__init__()
+#         self.base_url = base_url
+#         self.access_token_header = access_token_header
+#         self.refresh_token_header = refresh_token_header
+
+#     def request(self, method, url, *args, **kwargs):
+#         url = self.base_url + url
+#         if self.access_token_header:
+#             self.headers.update({self.access_token_header: kwargs.pop("access_token")})
+#         if self.refresh_token_header:
+#             self.headers.update({self.refresh_token_header: kwargs.pop("refresh_token")})
+#         return super().request(method, url, *args, **kwargs)
+
 session = DropbaseSession(base_url=base_url)
