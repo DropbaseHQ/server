60c3a5a4fab082fdbd977ef6d8006121d4449f1f
Trevor Du <trevor@dropbase.io>
2023-11-23 17:19:14 -0500

    test run_exec_task cases

--- server/tests/controllers/test_python.py
+++ server/tests/controllers/test_python.py
@@ -28,3 +28,90 @@ def test_run_exec_task():
     assert success
     assert stdout == "test output"
     assert output == None
+
+
+def test_run_exec_task_exception():
+    # Arrange
+    mock_child_conn = unittest.mock.MagicMock()
+    args = {
+        "app_name": "dropbase_test_app",
+        "page_name": "page1",
+        "python_string": "raise Exception",
+        "payload": {
+            "state": {"widgets": {"widget1": {}}, "tables": {"table1": {}}},
+            "context": {
+                "widgets": {"widget1": {"components": {}}},
+                "tables": {"table1": {"columns": {}}},
+            },
+        },
+        "file": {"name": "function1"},
+    }
+
+    # Act
+    run_exec_task(mock_child_conn, args)
+
+    # Assert
+    success, stdout, output = mock_child_conn.send.call_args.args[0]
+    assert not success
+
+
+def test_run_exec_task_last_var():
+    # Arrange
+    mock_child_conn = unittest.mock.MagicMock()
+    args = {
+        "app_name": "dropbase_test_app",
+        "page_name": "page1",
+        "python_string": "x=12\nx",
+        "payload": {
+            "state": {"widgets": {"widget1": {}}, "tables": {"table1": {}}},
+            "context": {
+                "widgets": {"widget1": {"components": {}}},
+                "tables": {"table1": {"columns": {}}},
+            },
+        },
+        "file": {"name": "function1"},
+    }
+
+    # Act
+    run_exec_task(mock_child_conn, args)
+
+    # Assert
+    success, stdout, output = mock_child_conn.send.call_args.args[0]
+    assert success
+    assert stdout == ""
+    assert output == 12
+
+
+def test_run_exec_task_last_var_is_df():
+    # Arrange
+    mock_child_conn = unittest.mock.MagicMock()
+    args = {
+        "app_name": "dropbase_test_app",
+        "page_name": "page1",
+        "python_string": "import pandas as pd\npd.DataFrame([[1]], columns=['?column?'])",
+        "payload": {
+            "state": {"widgets": {"widget1": {}}, "tables": {"table1": {}}},
+            "context": {
+                "widgets": {"widget1": {"components": {}}},
+                "tables": {"table1": {"columns": {}}},
+            },
+        },
+        "file": {"name": "function1"},
+    }
+
+    # Act
+    run_exec_task(mock_child_conn, args)
+
+    # Assert
+    success, stdout, output = mock_child_conn.send.call_args.args[0]
+    assert success
+    assert stdout == ""
+    assert output == {
+        "columns": ["?column?"],
+        "data": [[1]],
+    }
+
+
+def test_run_exec_task_last_var_is_context():
+    # TODO
+    return
