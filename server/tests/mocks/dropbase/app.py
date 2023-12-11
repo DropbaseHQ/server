dfc32ed256f809d2ca0a38ca9f0ab6ef81308d71
Trevor Du <trevor@dropbase.io>
2023-12-11 02:29:25 -0500

    test path traversal attack on delete app

--- server/tests/mocks/dropbase/app.py
+++ server/tests/mocks/dropbase/app.py
@@ -40,3 +40,15 @@ def delete_app_response(app_id: str):
             "id": app_id,
         }
     )
+
+
+def delete_app_response_path_traversal(app_name: str):
+    return lambda app_id: mock_response(
+        json={
+            "date": "2023-10-31T14:22:45.654115",
+            "name": app_name,
+            "workspace_id": "be5a519f-b9df-4f09-9f10-ce2f18d9a5ca",
+            "is_draft": False,
+            "id": app_id,
+        }
+    )
