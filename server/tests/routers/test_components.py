7be83bef0dfbf87bd28b2e5516368ccb16eada87
Trevor Du <trevor@dropbase.io>
2023-12-11 02:05:14 -0500

    add cases for all types in test_create_component_req

--- server/tests/routers/test_components.py
+++ server/tests/routers/test_components.py
@@ -8,7 +8,7 @@ from server.tests.verify_folder_structure import is_valid_folder_structure
 from server.tests.verify_object_exists import workspace_object_exists
 
 
-def test_create_component_req(test_client, dropbase_router_mocker):
+def test_create_component_req_text(test_client, dropbase_router_mocker):
     # Arrange
     dropbase_router_mocker.patch("component", "create_component", side_effect=create_component_response)
 
@@ -28,9 +28,86 @@ def test_create_component_req(test_client, dropbase_router_mocker):
     assert workspace_object_exists("Context", "widgets.widget1.components.test_text")
 
 
+def test_create_component_req_select(test_client, dropbase_router_mocker):
+    # Arrange
+    dropbase_router_mocker.patch("component", "create_component", side_effect=create_component_response)
+
+    data = {
+        "property": {
+            "name": "test_select",
+            "label": None,
+            "rules": None,
+            "default": None,
+            "required": None,
+            "on_change": None,
+            "validation": None
+        },
+        "widget_id": "abcdefg",
+        "type": "select",
+    }
+
+    # Act
+    res = test_client.post("/components", json=data)
+
+    # Assert
+    assert res.status_code == 200
+    assert is_valid_folder_structure()
+    assert workspace_object_exists("State", "widgets.widget1.test_select")
+    assert workspace_object_exists("Context", "widgets.widget1.components.test_select")
+
+
+def test_create_component_req_input(test_client, dropbase_router_mocker):
+    # Arrange
+    dropbase_router_mocker.patch("component", "create_component", side_effect=create_component_response)
+
+    data = {
+        "property": {
+            "name": "test_input",
+            "type": "text",
+            "label": None,
+            "rules": None,
+            "default": None,
+            "required": None,
+            "validation": None,
+            "placeholder": None
+        },
+        "widget_id": "abcdefg",
+        "type": "input",
+    }
+
+    # Act
+    res = test_client.post("/components", json=data)
+
+    # Assert
+    assert res.status_code == 200
+    assert is_valid_folder_structure()
+    assert workspace_object_exists("State", "widgets.widget1.test_input")
+    assert workspace_object_exists("Context", "widgets.widget1.components.test_input")
+
+
+def test_create_component_req_button(test_client, dropbase_router_mocker):
+    # Arrange
+    dropbase_router_mocker.patch("component", "create_component", side_effect=create_component_response)
+
+    data = {
+        "property": {"name": "test_button", "label": None, "visible": None, "on_click": None},
+        "widget_id": "abcdefg",
+        "type": "button",
+    }
+
+    # Act
+    res = test_client.post("/components", json=data)
+
+    # Assert
+    assert res.status_code == 200
+    assert is_valid_folder_structure()
+    assert workspace_object_exists("State", "widgets.widget1.test_button")
+    assert workspace_object_exists("Context", "widgets.widget1.components.test_button")
+
+
 def test_update_component_req(test_client, dropbase_router_mocker):
     # Arrange
-    test_create_component_req(test_client, dropbase_router_mocker)
+    test_create_component_req_text(test_client, dropbase_router_mocker)
     dropbase_router_mocker.patch("component", "update_component", side_effect=update_component_response)
 
     data = {
@@ -52,7 +129,7 @@ def test_update_component_req(test_client, dropbase_router_mocker):
 
 def test_delete_component_req(test_client, dropbase_router_mocker):
     # Arrange
-    test_create_component_req(test_client, dropbase_router_mocker)
+    test_create_component_req_text(test_client, dropbase_router_mocker)
     dropbase_router_mocker.patch("component", "delete_component", side_effect=delete_component_response)
 
     # Act
