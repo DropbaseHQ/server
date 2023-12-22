04dda1d64b681e48b88a9e93c26d20c78300250d
Jonathan <jon@dropbase.io>
2023-12-22 16:53:16 -0500

    Use update state context instead of handle state context

--- server/routers/widgets.py
+++ server/routers/widgets.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends
 
-from server.controllers.utils import handle_state_context_updates
+from server.controllers.utils import (
+    handle_state_context_updates,
+    update_state_context_files,
+)
 from server.requests.dropbase_router import DropbaseRouter, get_dropbase_router
 from server.schemas.widgets import CreateWidget, UpdateWidget
 
@@ -14,12 +17,12 @@ def create_widget_req(
     req: CreateWidget, router: DropbaseRouter = Depends(get_dropbase_router)
 ):
     resp = router.widget.create_widget(req.dict())
-    state_context = resp.json().get("state_context")
-    handle_state_context_updates(resp)
-    return {
-        "widget": resp.json().get("widget"),
-        "state_context": state_context,
-    }
+    resp = resp.json()
+    widget = resp.get("widget")
+    if "state_context" in resp:
+        resp = resp.get("state_context")
+    update_state_context_files(**resp)
+    return {"widget": widget}
 
 
 @router.put("/{widget_id}/")
