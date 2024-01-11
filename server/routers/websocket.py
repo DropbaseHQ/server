059f4c63e5f497409d6f113dc269f52360169e7d
Jonathan <jon@dropbase.io>
2024-01-11 14:35:08 -0500

    Refactor: Websocket auth done on first message rather than handshake

--- server/routers/websocket.py
+++ server/routers/websocket.py
@@ -2,21 +2,33 @@ from fastapi import APIRouter, WebSocket
 from server.controllers.display_rules import run_display_rule
 from server.constants import DROPBASE_API_URL
 import requests
+import websockets
 
 router = APIRouter()
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    response = requests.post(
-        DROPBASE_API_URL + "/worker/verify_token", cookies=websocket.cookies
-    )
-    if response.status_code != 200:
-        await websocket.close()
-        return
     await websocket.accept()
     while True:
         data = await websocket.receive_json()
+
+        # ======== AUTHENTICATION ======== #
+        if not hasattr(websocket, "authenticated") or not websocket.authenticated:
+            if data["type"] == "auth":
+                access_token = data["access_token"]
+                response = requests.post(
+                    DROPBASE_API_URL + "/worker/verify_token",
+                    cookies={"access_token_cookie": access_token},
+                )
+                if response.status_code == 200:
+                    setattr(websocket, "authenticated", True)
+                    await websocket.send_json({"authenticated": True})
+                else:
+                    await websocket.send_json({"authenticated": False})
+            else:
+                await websocket.send_json({"message": "You are not authenticated"})
+            continue
         if data["type"] == "display_rule":
             state_context = data["state_context"]
             state = state_context["state"]
@@ -30,4 +42,5 @@ async def websocket_endpoint(websocket: WebSocket):
             context = run_display_rule(**payload)
             await websocket.send_json({"context": context})
         else:
-            await websocket.send_json({"message": f"You sent: {data['message']}"})
+            message = data.get("message")
+            await websocket.send_json({"message": f"You sent: {message}"})
