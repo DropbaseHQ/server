79b3db984c14ffb0ea35cf8e6750feedafa80aa1
Ayazhan Zhakhan <az@dropbase.io>
2024-01-11 17:47:51 -0500

    refactored file routes and controllers

--- server/controllers/files.py
+++ server/controllers/files.py
@@ -1,26 +1,28 @@
-from server.constants import cwd
-from server.controllers.utils import read_page_properties, write_page_properties
-from server.schemas.files import CreateFile
+import glob
+import os
+import re
+
+from server.constants import FILE_NAME_REGEX, cwd
+from server.controllers.query import get_sql_variables
+from server.controllers.utils import read_page_properties, rename_function_in_file, write_page_properties
+from server.schemas.files import CreateFile, DeleteFile, RenameFile, UpdateFile
 
 
 def create_file(req: CreateFile):
-    try:
-        # create file in a local workspace
-        file_name = req.name + ".sql" if req.type == "sql" else req.name + ".py"
-        path = cwd + f"/workspace/{req.app_name}/{req.page_name}/scripts/{file_name}"
-        boilerplate_code = compose_boilerplate_code(req)
+    # create file in a local workspace
+    file_name = req.name + ".sql" if req.type == "sql" else req.name + ".py"
+    path = cwd + f"/workspace/{req.app_name}/{req.page_name}/scripts/{file_name}"
+    boilerplate_code = compose_boilerplate_code(req)
 
-        with open(path, "a") as f:
-            f.write(boilerplate_code)
+    with open(path, "a") as f:
+        f.write(boilerplate_code)
 
-        # update properties.json
-        properties = read_page_properties(req.app_name, req.page_name)
-        properties["files"].append({"name": req.name, "type": req.type, "source": req.source})
-        write_page_properties(req.app_name, req.page_name, properties)
+    # update properties.json
+    properties = read_page_properties(req.app_name, req.page_name)
+    properties["files"].append({"name": req.name, "type": req.type, "source": req.source})
+    write_page_properties(req.app_name, req.page_name, properties)
 
-        return 200, {"status": "success"}
-    except Exception as e:
-        return 500, str(e)
+    return {"status": "success"}
 
 
 def compose_boilerplate_code(req: CreateFile):
@@ -45,3 +47,90 @@ def function():
 """
     else:
         return ""
+
+
+def rename_file(req: RenameFile):
+    file_ext = ".sql" if req.type == "sql" else ".py"
+    file_name = req.old_name + file_ext
+    file_path = cwd + f"/workspace/{req.app_name}/{req.page_name}/scripts/{file_name}"
+
+    # if file does not exist, exit
+    if not os.path.exists(file_path):
+        raise Exception("The file does not exist")
+
+    new_file_name = req.new_name + file_ext
+    new_path = cwd + f"/workspace/{req.app_name}/{req.page_name}/scripts/{new_file_name}"
+    if os.path.exists(file_path):
+        os.rename(file_path, new_path)
+    rename_function_in_file(file_path=new_path, old_name=req.old_name, new_name=req.new_name)
+
+    # update properties.json
+    properties = read_page_properties(req.app_name, req.page_name)
+
+    for file in properties["files"]:
+        if file["name"] == req.old_name:
+            file["name"] = req.new_name
+            break
+
+    write_page_properties(req.app_name, req.page_name, properties)
+
+    return {"status": "success"}
+
+
+def update_file(function_name: str, req: UpdateFile):
+    file_extension = ".sql" if req.type == "sql" else ".py"
+    file_name = function_name + file_extension
+
+    # update file content
+    file_path = cwd + f"/workspace/{req.app_name}/{req.page_name}/scripts/{file_name}"
+    with open(file_path, "w") as f:
+        f.write(req.sql)
+
+    # update properties.json
+    properties = read_page_properties(req.app_name, req.page_name)
+
+    depends_on = []
+    if req.type == "sql":
+        # update depends on flags in properties.json
+        depends_on = get_sql_variables(user_sql=req.sql)
+
+    # update file property in properties.json
+    for file in properties["files"]:
+        if file["name"] == req.name:
+            file["source"] = req.source
+            file["type"] = req.type
+            file["depends_on"] = depends_on
+            break
+
+    write_page_properties(req.app_name, req.page_name, properties)
+    return {"status": "success"}
+
+
+def delete_file(req: DeleteFile):
+    function_name = req.file_name[:-4] if req.type == "sql" else req.file_name[:-3]
+    path = cwd + f"/workspace/{req.app_name}/{req.page_name}/scripts/{req.file_name}"
+    if not os.path.exists(path):
+        raise Exception("The file does not exist")
+
+    os.remove(path)
+
+    properties = read_page_properties(req.app_name, req.page_name)
+
+    for file in properties["files"]:
+        if file["name"] == function_name:
+            properties["files"].remove(file)
+            break
+
+    write_page_properties(req.app_name, req.page_name, properties)
+
+    return {"status": "success"}
+
+
+def get_all_files(app_name: str, page_name: str):
+    if not (re.match(FILE_NAME_REGEX, app_name) and re.match(FILE_NAME_REGEX, page_name)):
+        raise Exception("No files found. Please check if the app name and page name are valid.")
+    dir_path = cwd + f"/workspace/{app_name}/{page_name}/scripts"
+    py_files = glob.glob(os.path.join(dir_path, "*.py"))
+    py_files = [file for file in py_files if not file.endswith("__init__.py")]
+    sql_files = glob.glob(os.path.join(dir_path, "*.sql"))
+    return {"files": py_files + sql_files}
--- server/controllers/page.py
+++ server/controllers/page.py
@@ -2,25 +2,14 @@ from pydantic import BaseModel
 
 from server.controllers.display_rules import run_display_rule
 from server.controllers.generate_models import create_state_context_files
-from server.controllers.utils import (
-    get_state_context_model,
-    read_page_properties,
-    validate_column_name,
-    write_page_properties,
-)
+from server.controllers.utils import get_state_context_model, validate_column_name, write_page_properties
 from server.schemas.page import PageProperties
 
 
 def update_page_properties(req: PageProperties):
     # TODO: revert to prev version if failed
     # validate properties
-    validate_property(req.properties.dict())
-
-    # to update depends on flags in properties.json
-    properties = read_page_properties(req.app_name, req.page_name)
-    # check for a data fetcher is bound to a table
-    check_data_fetcher_bound_to_table(properties, req.properties.dict())
-
+    validate_property_names(req.properties.dict())
     # write properties
     write_page_properties(**req.dict())
     # update state context
@@ -29,7 +18,7 @@ def update_page_properties(req: PageProperties):
     return get_page_state_context(req.app_name, req.page_name)
 
 
-def validate_property(properties: dict):
+def validate_property_names(properties: dict):
     # validate column names
     for table in properties["tables"]:
         if not validate_column_name(table["name"]):
@@ -44,20 +33,6 @@ def validate_property(properties: dict):
         for component in widget.get("components"):
             if not validate_column_name(component["name"]):
                 raise Exception("Invalid component names present in the table")
-
-
-def check_data_fetcher_bound_to_table(current_propertirs: dict, incoming_properties: dict):
-    # check if data fetcher has changes
-    current_tables = {table["name"]: table for table in current_propertirs["tables"]}
-    current_files = {file["name"]: file for file in current_propertirs["files"]}
-    for table in incoming_properties["tables"]:
-        if table["name"] in current_tables.keys():
-            if table.get("fetcher") != current_tables[table["name"]].get("fetcher"):
-                if current_files[table.get("fetcher")].get("type") == "sql":
-                    # update depends on flags in properties.json
-                    # depends_on = get_sql_variables(user_sql=req.sql)
-                    # tbc
-                    pass
 
 
 def get_page_state_context(app_name: str, page_name: str):
