3407c43a02e0877fe5afbebe90f68105709e2c2c
Jonathan <jon@dropbase.io>
2024-01-17 16:03:42 -0500

    Refactor run python query to deal with df and tuple

--- server/controllers/run_python.py
+++ server/controllers/run_python.py
@@ -9,17 +9,37 @@ from server.controllers.utils import (
 )
 from server.schemas.files import DataFile
 from server.schemas.table import FilterSort
+import pandas as pd
 
 
-def run_python_query(app_name, page_name, file: dict, state: dict, filter_sort: FilterSort):
+def run_python_query(
+    app_name, page_name, file: dict, state: dict, context: dict, filter_sort: FilterSort
+):
     try:
-        state = get_state(app_name, page_name, state)
-        args = {"state": state, "filter_sort": filter_sort}
+        filled_state = get_state(app_name, page_name, state)
+        _, filled_context = get_state_context(app_name, page_name, state, context)
+        args = {
+            "state": filled_state,
+            "context": filled_context,
+            "filter_sort": filter_sort,
+        }
         file = DataFile(**file)
         function_name = get_data_function_by_file(app_name, page_name, file)
-        df = call_function(function_name, **args)
-        df = clean_df(df)
-        return convert_df_to_resp_obj(df), 200
+        function_result = call_function(function_name, **args)
+
+        if isinstance(function_result, pd.DataFrame):
+            df = function_result
+            df = clean_df(df)
+            return convert_df_to_resp_obj(df), 200
+        else:
+            df, context = function_result
+
+            df = clean_df(df)
+            return {
+                "dataframe": convert_df_to_resp_obj(df),
+                "context": context,
+            }, 200
+
     except Exception as e:
         return {"error": str(e)}, 500
 
