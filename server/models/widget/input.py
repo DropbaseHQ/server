71796e3a6bdfafe999f6af143e6054eccb20e8a0
Param Thakkar <pht0104@gmail.com>
2024-01-19 12:31:07 -0500

    ensure that input's data_field is required

--- server/models/widget/input.py
+++ server/models/widget/input.py
@@ -1,6 +1,6 @@
 from typing import Annotated, Any, List, Literal, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from server.models.category import PropertyCategory
 from server.models.common import ComponentDisplayProperties
@@ -9,7 +9,7 @@ from server.models.common import ComponentDisplayProperties
 class InputBaseProperties(BaseModel):
     label: Annotated[str, PropertyCategory.default]
     name: Annotated[str, PropertyCategory.default]
-    data_type: Annotated[Literal["text", "integer", "float", "date"], PropertyCategory.default] = "text"
+    data_type: Annotated[Literal["text", "integer", "float", "date"], PropertyCategory.default]
     placeholder: Annotated[Optional[str], PropertyCategory.default]
     default: Annotated[Optional[Any], PropertyCategory.default]
 
@@ -18,6 +18,10 @@ class InputBaseProperties(BaseModel):
 
     # internal
     component_type: Literal["input"]
+
+    def __init__(self, **data):
+        data.setdefault("data_type", "text")
+        super().__init__(**data)
 
 
 class InputSharedProperties(BaseModel):
