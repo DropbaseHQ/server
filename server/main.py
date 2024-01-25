54a2c1fb379bc75f037491c8e0c964addf49712f
Jonathan <jon@dropbase.io>
2024-01-25 12:00:50 -0500

    Add log filter for health checks

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 import asyncio
-
+import logging
 import requests
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -30,6 +30,20 @@ class NoCacheStaticFiles(StaticFiles):
         response.headers["Cache-Control"] = "no-store"
         return response
 
+
+spam_urls = ["/health/"]
+
+
+class LogSpamFilter(logging.Filter):
+    def filter(self, record):
+        for url in spam_urls:
+            if url in record.args:
+                return False
+        return True
+
+
+logger = logging.getLogger("uvicorn.access")
+logger.addFilter(LogSpamFilter())
 
 app = FastAPI()
 origins = ["http://localhost:3030", "http://www.localhost:3030"]
@@ -63,7 +77,10 @@ app.include_router(websocket_router)
 # send health report to dropbase server
 async def send_report_continuously():
     while True:
-        requests.get(DROPBASE_API_URL + f"/worker/worker_status/{DROPBASE_TOKEN}/{WORKER_VERSION}")
+        requests.get(
+            DROPBASE_API_URL
+            + f"/worker/worker_status/{DROPBASE_TOKEN}/{WORKER_VERSION}"
+        )
         await asyncio.sleep(300)
 
 
