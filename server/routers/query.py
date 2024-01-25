bee64829943e955d58b9bf132ce7765f6a520364
Jonathan <jon@dropbase.io>
2024-01-25 16:45:11 -0500

    Add auth to query endpoint

--- server/routers/query.py
+++ server/routers/query.py
@@ -1,13 +1,16 @@
 import sqlalchemy.exc
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 
 from server.controllers.query import run_query
 from server.schemas.run_python import QueryPythonRequest
+from server.auth.dependency import EnforceUserAppPermissions
 
-router = APIRouter(prefix="/query", tags=["query"], responses={404: {"description": "Not found"}})
+router = APIRouter(
+    prefix="/query", tags=["query"], responses={404: {"description": "Not found"}}
+)
 
 
-@router.post("/")
+@router.post("/", dependencies=[Depends(EnforceUserAppPermissions(action="use"))])
 async def run_query_req(req: QueryPythonRequest):
     try:
         return run_query(req)
