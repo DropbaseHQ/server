61bb17c9fe71697fc524649b22e0a3f54dec4ae4
stephen-ics <stephen@dropbase.io>
2024-01-30 11:39:45 -0500

    small bug fix

--- server/controllers/utils.py
+++ server/controllers/utils.py
@@ -70,10 +70,10 @@ def connect_to_user_db(source_name: str):
     CredsClass = db_type_to_class.get(creds.get("type"))
     creds = CredsClass(**creds)
 
-    if(creds_type == "PG"):
+    if(creds_type == "pg" or creds_type == "postgres"):
         SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{creds.username}:{creds.password}@{creds.host}:{creds.port}/{creds.database}"  # noqa
         return create_engine(SQLALCHEMY_DATABASE_URL, future=True)
-    elif(creds_type == "MYSQL"):
+    elif(creds_type == "mysql"):
         SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{creds.username}:{creds.password}@{creds.host}:{creds.port}/{creds.database}"
         return create_engine(SQLALCHEMY_DATABASE_URL, future=True)
 
