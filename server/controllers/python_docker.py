1cfcc355e6d00c0805c72a876701fbe887e04e05
Ayazhan Zhakhan <az@dropbase.io>
2024-01-31 16:10:44 -0500

    set auto_remove=True to start docker to allow for longer running tasks

--- server/controllers/python_docker.py
+++ server/controllers/python_docker.py
@@ -14,19 +14,12 @@ def run_container(env_vars: dict, docker_script: str = "inside_docker"):
     mount2 = docker.types.Mount(target="/app", source=worker_files, type="bind")
 
     # Run the Docker container with the mount
-    container = client.containers.run(
+    client.containers.run(
         "worker",
         command=f"python {docker_script}.py",
         mounts=[mount1, mount2],
         environment=env_vars,  # pass environment variables here
         detach=True,
         working_dir="/app",  # set working directory as /app
+        auto_remove=True,
     )
-
-    # time.sleep(1)
-    # # Fetch and print container's log
-    # logs = container.logs()
-    # print(logs.decode("utf-8"))  # Decode bytes to string format.
-
-    container.stop()
-    container.remove()
