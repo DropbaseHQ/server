d76c61a938e45f674fc214455b30d82e6f3308e8
Ayazhan Zhakhan <az@dropbase.io>
2024-01-31 16:52:29 -0500

    fixed query tests

--- server/tests/routers/test_run_python.py
+++ server/tests/routers/test_run_python.py
@@ -1,23 +1,39 @@
+from server.tests.constants import TEST_APP_NAME, TEST_PAGE_NAME
+
+
 def test_run_python_string(test_client):
     # Arrange
     data = {
-        "app_name": "dropbase_test_app",
-        "page_name": "page1",
-        "python_string": "print(0)\nx=3\nprint(1)\nprint(2)\nprint(x)",
-        "payload": {
-            "state": {"widgets": {"widget1": {}}, "tables": {"table1": {}}},
-            "context": {
-                "widgets": {"widget1": {"components": {}}},
-                "tables": {"table1": {"columns": {}}},
+        "file_code": f"""import pandas as pd
+from workspace.{TEST_APP_NAME}.{TEST_PAGE_NAME} import State, Context
+
+def test_data_fetcher(state: State) -> pd.DataFrame:
+    return pd.DataFrame(data=[[1]], columns=["x"])""",
+        "test_code": "test_data_fetcher(state)",
+        "state": {"tables": {"table1": {}}, "widgets": {}},
+        "context": {
+            "tables": {
+                "table1": {"message": None, "message_type": None, "reload": False, "columns": {}}
             },
+            "widgets": {},
         },
-        "file": {"name": "function1"},
     }
 
     # Act
     res = test_client.post("/query/python_string", json=data)
 
     # Assert
+    assert res.status_code == 202
+    response_data = res.json()
+    job_id = response_data["job_id"]
+
+    import time
+
+    time.sleep(2)
+
+    res = test_client.get(f"/query/status/{job_id}")
     assert res.status_code == 200
-    assert res.json()["success"]
-    assert res.json()["stdout"] == "0\n1\n2\n3\n"
+    res_data = res.json()
+    assert res_data["type"] == "table"
+    assert res_data["columns"] == [{"name": "x", "column_type": "int64", "display_type": "integer"}]
+    assert res_data["data"] == [[1]]
