5a5ad4251dd5d7fad433738fd64eb1b036abedfb
Ayazhan Zhakhan <az@dropbase.io>
2024-01-31 19:15:38 -0500

    updated docker build for worker task

--- server/controllers/python_docker.py
+++ server/controllers/python_docker.py
@@ -10,21 +10,19 @@ def run_container(env_vars: dict, docker_script: str = "inside_docker"):
     client = docker.from_env()
 
     # add environment variables from .env file
+    # TODO: revisit this, should only send .env variables to docker
     config = {key: os.getenv(key) for key in os.environ.keys()}
     env_vars = {**env_vars, **config}
 
     # mount workspace directory
     workspace_dir = cwd + "/workspace"
     mount1 = docker.types.Mount(target="/app/workspace", source=workspace_dir, type="bind")
-    # mount worker files
-    worker_files = cwd + "/worker"
-    mount2 = docker.types.Mount(target="/app", source=worker_files, type="bind")
 
     # Run the Docker container with the mount
     client.containers.run(
         "worker",
         command=f"python {docker_script}.py",
-        mounts=[mount1, mount2],
+        mounts=[mount1],
         environment=env_vars,  # pass environment variables here
         detach=True,
         working_dir="/app",  # set working directory as /app
