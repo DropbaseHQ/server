9dc9a7f55585e1df03d4ce735511af8c85766192
stephen-ics <stephen@dropbase.io>
2024-02-02 12:30:38 -0500

    postgres implementation of base db class

--- server/constants.py
+++ server/constants.py
@@ -14,3 +14,38 @@ INFER_TYPE_SAMPLE_SIZE = 50
 
 FILE_NAME_REGEX = re.compile(r"^[A-Za-z0-9_.]+$")
 WORKER_VERSION = "0.2.0"
+
+pg_base_type_mapper = {
+    "TEXT": "text",
+    "VARCHAR": "text",
+    "CHAR": "text",
+    "CHARACTER": "text",
+    "STRING": "text",
+    "BINARY": "text",
+    "VARBINARY": "text",
+    "INTEGER": "integer",
+    "INT": "integer",
+    "BIGINT": "integer",
+    "SMALLINT": "integer",
+    "TINYINT": "integer",
+    "BYTEINT": "integer",
+    "REAL": "float",
+    "FLOAT": "float",
+    "FLOAT4": "float",
+    "FLOAT8": "float",
+    "DOUBLE": "float",
+    "DOUBLE PRECISION": "float",
+    "DECIMAL": "float",
+    "NUMERIC": "float",
+    "BOOLEAN": "boolean",
+    "DATE": "date",
+    "TIME": "time",
+    "DATETIME": "datetime",
+    "TIMESTAMP": "datetime",
+    "TIMESTAMP_LTZ": "datetime",
+    "TIMESTAMP_NTZ": "datetime",
+    "TIMESTAMP_TZ": "datetime",
+    "VARIANT": "text",
+    "OBJECT": "text",
+    "ARRAY": "text",
+}
--- server/controllers/tables.py
+++ server/controllers/tables.py
@@ -7,6 +7,7 @@ from server.controllers.validation import validate_smart_cols
 from server.requests.dropbase_router import DropbaseRouter
 from server.schemas.files import DataFile
 from server.schemas.table import ConvertTableRequest
+from server.constants import pg_base_type_mapper
 
 
 def convert_sql_table(req: ConvertTableRequest, router: DropbaseRouter):
@@ -63,39 +64,3 @@ def convert_sql_table(req: ConvertTableRequest, router: DropbaseRouter):
 
     except Exception as e:
         return str(e), 500
-
-
-pg_base_type_mapper = {
-    "TEXT": "text",
-    "VARCHAR": "text",
-    "CHAR": "text",
-    "CHARACTER": "text",
-    "STRING": "text",
-    "BINARY": "text",
-    "VARBINARY": "text",
-    "INTEGER": "integer",
-    "INT": "integer",
-    "BIGINT": "integer",
-    "SMALLINT": "integer",
-    "TINYINT": "integer",
-    "BYTEINT": "integer",
-    "REAL": "float",
-    "FLOAT": "float",
-    "FLOAT4": "float",
-    "FLOAT8": "float",
-    "DOUBLE": "float",
-    "DOUBLE PRECISION": "float",
-    "DECIMAL": "float",
-    "NUMERIC": "float",
-    "BOOLEAN": "boolean",
-    "DATE": "date",
-    "TIME": "time",
-    "DATETIME": "datetime",
-    "TIMESTAMP": "datetime",
-    "TIMESTAMP_LTZ": "datetime",
-    "TIMESTAMP_NTZ": "datetime",
-    "TIMESTAMP_TZ": "datetime",
-    "VARIANT": "text",
-    "OBJECT": "text",
-    "ARRAY": "text",
-}
