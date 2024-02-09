52cc4e4e0aa524498bd59cc093261f3c376cf385
stephen-turtles <stephen@dropbase.io>
2024-02-09 13:53:57 -0500

    fuxed app_rename_duplicate test

--- server/routers/app.py
+++ server/routers/app.py
@@ -3,17 +3,12 @@ import os
 from fastapi import APIRouter, Depends, Response
 
 from server.controllers.app import get_workspace_apps
-from server.controllers.utils import (
-    check_if_object_exists,
-    validate_column_name,
-)
+from server.controllers.utils import check_if_object_exists, validate_column_name
 from server.controllers.workspace import AppFolderController, WorkspaceFolderController
 from server.requests.dropbase_router import DropbaseRouter, get_dropbase_router
 from server.schemas.workspace import CreateAppRequest, RenameAppRequest
 
-router = APIRouter(
-    prefix="/app", tags=["app"], responses={404: {"description": "Not found"}}
-)
+router = APIRouter(prefix="/app", tags=["app"], responses={404: {"description": "Not found"}})
 
 
 @router.get("/list/")
@@ -29,9 +24,7 @@ def create_app_req(
 ):
     if not validate_column_name(req.app_name):
         response.status_code = 400
-        return {
-            "message": "Invalid app name. Only alphanumeric characters and underscores are allowed"
-        }
+        return {"message": "Invalid app name. Only alphanumeric characters and underscores are allowed"}
 
     # assert page does not exist
     if check_if_object_exists(f"workspace/{req.app_name}/"):
@@ -43,9 +36,8 @@ def create_app_req(
         app_folder_controller = AppFolderController(
             app_name=req.app_name, r_path_to_workspace=r_path_to_workspace
         )
-        return app_folder_controller.create_app(
-            workspace_id=req.workspace_id, router=router
-        )
+
+        return app_folder_controller.create_app(workspace_id=req.workspace_id, router=router)
     except Exception as e:
         response.status_code = 500
         return {"error": str(e)}
@@ -54,10 +46,9 @@ def create_app_req(
 @router.put("/")
 def rename_app_req(req: RenameAppRequest, response: Response):
     # assert page does not exist
+
     path_to_workspace = os.path.join(os.path.dirname(__file__), "../../workspace")
-    workspace_folder_controller = WorkspaceFolderController(
-        r_path_to_workspace=path_to_workspace
-    )
+    workspace_folder_controller = WorkspaceFolderController(r_path_to_workspace=path_to_workspace)
 
     target_app = workspace_folder_controller.get_app(app_id=req.app_id)
     if target_app is None:
@@ -68,9 +59,9 @@ def rename_app_req(req: RenameAppRequest, response: Response):
         app_id=req.app_id, app_info={**target_app, "label": req.new_label}
     )
 
-    # if check_if_object_exists(f"workspace/{req.new_name}/"):
-    #     response.status_code = 400
-    #     return {"message": "An app with this name already exists"}
+    if check_if_object_exists(f"workspace/{req.new_name}/"):
+        response.status_code = 400
+        return {"message": "An app with this name already exists"}
 
     # workspace_folder_path = os.path.join(os.path.dirname(__file__), "../../workspace")
     # app_path = os.path.join(workspace_folder_path, req.old_name)
@@ -88,6 +79,4 @@ def delete_app_req(
 ):
     r_path_to_workspace = os.path.join(os.path.dirname(__file__), "../../workspace")
     app_folder_controller = AppFolderController(app_name, r_path_to_workspace)
-    return app_folder_controller.delete_app(
-        app_name=app_name, response=response, router=router
-    )
+    return app_folder_controller.delete_app(app_name=app_name, response=response, router=router)
