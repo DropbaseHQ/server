06c1faf08b582a93a839f869e451ec5781b3c9d8
Ayazhan Zhakhan <az@dropbase.io>
2024-02-14 16:45:37 -0500

    replaced loading env vars with load_dotenv

--- server/constants.py
+++ server/constants.py
@@ -1,13 +1,17 @@
 import os
 
+from dotenv import load_dotenv
+
 from dropbase.database.sources import get_sources
+
+load_dotenv()
 
 cwd = os.getcwd()
 
 
-DROPBASE_TOKEN = os.getenv("DROPBASE_TOKEN")
-DROPBASE_API_URL = os.getenv("DROPBASE_API_URL") or "https://api.dropbase.io"
-TASK_TIMEOUT = os.getenv("TASK_TIMEOUT") or 60
+DROPBASE_TOKEN = os.environ.get("DROPBASE_TOKEN")
+DROPBASE_API_URL = os.environ.get("DROPBASE_API_URL") or "https://api.dropbase.io"
+TASK_TIMEOUT = os.environ.get("TASK_TIMEOUT") or 60
 DATA_PREVIEW_SIZE = 100
 INFER_TYPE_SAMPLE_SIZE = 50
 WORKSPACE_SOURCES = get_sources()
