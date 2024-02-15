35687833a6b2f1c1b0719f86b594f042b3ff733e
stephen-turtles <stephen@dropbase.io>
2024-02-15 14:43:23 -0500

    all snowflake related tests pass

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -66,12 +66,15 @@ def snowflake_db():
 
     # Create a new database for testing and use it
     test_db_name = "test_db"
+    test_schema_name = "PUBLIC"
+
+    conn.cursor().execute(f"DROP DATABASE IF EXISTS {test_db_name}")
     conn.cursor().execute(f"CREATE DATABASE IF NOT EXISTS {test_db_name}")
     conn.cursor().execute(f"USE DATABASE {test_db_name}")
+    conn.cursor().execute(f"CREATE SCHEMA IF NOT EXISTS {test_schema_name}")
 
     yield conn  # This allows the test to run with the connection
 
-    # Tear down: drop the database after tests are done
     conn.cursor().execute(f"DROP DATABASE IF EXISTS {test_db_name}")
     conn.close()
 
