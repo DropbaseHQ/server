b576d469371d18f571081373751633575101ead5
stephen-turtles <stephen@dropbase.io>
2024-02-15 14:48:30 -0500

    added in env credentials

--- server/tests/constants.py
+++ server/tests/constants.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 TEST_APP_NAME = "dropbase_test_app"
@@ -8,3 +9,25 @@ WORKSPACE_PATH = ROOT_PATH.joinpath("workspace")
 TEMPDIR_PATH = ROOT_PATH.joinpath(".temp")
 DEMO_INIT_SQL_PATH = ROOT_PATH.joinpath("demo/init.sql")
 DEMO_SNOWFLAKE_INIT_SQL_PATH = ROOT_PATH.joinpath("demo/init_snowflake.sql")
+
+SNOWFLAKE_TEST_CONNECTION_PARAMS = {
+    "account": os.getenv("SNOWFLAKE_TEST_HOST"),
+    "user": os.getenv("SNOWFLAKE_TEST_USERNAME"),
+    "password": os.getenv("SNOWFLAKE_TEST_PASSWORD"),
+    "database": os.getenv("SNOWFLAKE_TEST_DATABASE"),
+    "schema": os.getenv("SNOWFLAKE_TEST_DBSCHEMA"),
+    "warehouse": os.getenv("SNOWFLAKE_TEST_WAREHOUSE"),
+    "role": os.getenv("SNOWFLAKE_TEST_ROLE"),
+}
+
+
+SNOWFLAKE_TEST_CREDS = {
+    "drivername": "snowflake",
+    "host": os.getenv("SNOWFLAKE_TEST_HOST"),
+    "username": os.getenv("SNOWFLAKE_TEST_USERNAME"),
+    "password": os.getenv("SNOWFLAKE_TEST_PASSWORD"),
+    "database": os.getenv("SNOWFLAKE_TEST_DATABASE"),
+    "dbschema": os.getenv("SNOWFLAKE_TEST_DBSCHEMA"),
+    "warehouse": os.getenv("SNOWFLAKE_TEST_WAREHOUSE"),
+    "role": os.getenv("SNOWFLAKE_TEST_ROLE"),
+}
