44f0fa9da98d41434c06195ba18acef456408243
stephen-turtles <stephen@dropbase.io>
2024-02-20 15:33:40 -0500

    pushed temp solution to edit cells

--- dropbase/src/dropbase/schemas/edit_cell.py
+++ dropbase/src/dropbase/schemas/edit_cell.py
@@ -7,7 +7,7 @@ from dropbase.schemas.files import DataFile
 
 class CellProps(BaseModel):
     name: str
-    schema_name: str
+    schema_name: str = "public"
     table_name: str
     column_name: str
     edit_keys: List[str]
@@ -15,7 +15,7 @@ class CellProps(BaseModel):
 
 class CellEdit(BaseModel):
     column_name: str
-    data_type: str
+    data_type: str = "VARCHAR"  # pushed a temporary solution that sets a default value so no validation error is thrown
     old_value: Any
     new_value: Any
     row: dict
