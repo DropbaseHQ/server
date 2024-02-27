eabee7250a3e3c411899f05b7b1318756c88cdb8
Param Thakkar <param@dropbase.io>
2024-02-27 16:15:57 -0500

    Separate apply_filters functions

--- dropbase/src/dropbase/database/databases/postgres.py
+++ dropbase/src/dropbase/database/databases/postgres.py
@@ -292,9 +292,14 @@ class PostgresDatabase(Database):
                         continue
 
                 filter_values[filter_value_name] = filter.value
-                filters_list.append(
-                    f'user_query."{filter.column_name}" {filter.condition} :{filter_value_name}'
-                )
+                if filter.column_type == "text":
+                    filters_list.append(
+                        f'LOWER(user_query."{filter.column_name}") {filter.condition} LOWER(:{filter_value_name})'
+                    )
+                else:
+                    filters_list.append(
+                        f'user_query."{filter.column_name}" {filter.condition} :{filter_value_name}'
+                    )
 
             filter_sql += " AND ".join(filters_list)
         filter_sql += "\n"
