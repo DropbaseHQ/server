04e4589a402ae44a136454371cd496ef84c0a895
stephen-turtles <155136867+stephen-turtles@users.noreply.github.com>
2024-02-28 19:00:48 -0500

    fixed property schema displays (#131)
    * fixed property schema displays
    
    * added support for non-schema dbs
    
    * replaces dbschema with schema in env vars
    
    * fixed edit cells for snowflake temporarily

--- dropbase/src/dropbase/database/connect.py
+++ dropbase/src/dropbase/database/connect.py
@@ -11,18 +11,19 @@ def connect_to_user_db(name: str):
     creds = WORKSPACE_SOURCES.get(name)
     creds_fields = creds.get("fields")
 
+    creds_dict = creds_fields.dict()
     schema_name = "public"
 
     match creds.get("type"):
         case "postgres":
-            return PostgresDatabase(creds_fields.dict(), schema=schema_name)
+            return PostgresDatabase(creds_dict, schema=schema_name)
         case "pg":
-            return PostgresDatabase(creds_fields.dict(), schema=schema_name)
+            return PostgresDatabase(creds_dict, schema=schema_name)
         case "mysql":
-            return MySqlDatabase(creds_fields.dict())
+            return MySqlDatabase(creds_dict)
         case "snowflake":
-            return SnowflakeDatabase(creds_fields.dict())
+            return SnowflakeDatabase(creds_dict, schema=schema_name)
         case "sqlite":
-            return SqliteDatabase(creds_fields.dict())
+            return SqliteDatabase(creds_dict)
         case _:
             raise Exception(f"Database type {creds_fields.get('type')} not supported")
