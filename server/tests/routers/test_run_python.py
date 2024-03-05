d4b24ee29727a6506ea47337dca5e036cfcc1fd8
stephen-turtles <stephen@dropbase.io>
2024-03-05 15:21:00 -0500

    directly used run in inside_docker to test python string queries

--- server/tests/routers/test_run_python.py
+++ server/tests/routers/test_run_python.py
@@ -1,40 +1,72 @@
+import importlib
+import json
+import os
+import time
+from unittest.mock import patch
+
+import pytest
+import redis
+from dotenv import load_dotenv
+
 from server.tests.constants import TEST_APP_NAME, TEST_PAGE_NAME
 
+load_dotenv()
 
-def test_run_python_string(test_client):
+
+@pytest.fixture(scope="session")
+def setup_redis():
+    r = redis.Redis(host="localhost", port=6379, db=0)
+    return r
+
+
+def test_run_python_string(setup_redis):
     # Arrange
-    data = {
-        "file_code": f"""import pandas as pd
-from workspace.{TEST_APP_NAME}.{TEST_PAGE_NAME} import State, Context
+    os.environ["type"] = "string"
+    job_id = "test_job_id"
+    response = {
+        "stdout": "",
+        "traceback": "",
+        "message": "",
+        "type": "",
+        "status_code": 202,
+        "job_id": job_id,
+    }
 
-def test_data_fetcher(state: State) -> pd.DataFrame:
-    return pd.DataFrame(data=[[1]], columns=["x"])""",
-        "test_code": "test_data_fetcher(state)",
-        "state": {"tables": {"table1": {}}, "widgets": {}},
-        "context": {
+    # Set env vars to use the run function
+    os.environ["job_id"] = job_id
+    os.environ[
+        "file_code"
+    ] = 'import pandas as pd\ndef test_data_fetcher(): return pd.DataFrame(data=[[1]], columns=["x"])'
+    os.environ["test_code"] = "test_data_fetcher()"
+    os.environ["state"] = json.dumps({"tables": {"table1": {}}, "widgets": {}})
+    os.environ["context"] = json.dumps(
+        {
             "tables": {
                 "table1": {"message": None, "message_type": None, "reload": False, "columns": {}}
             },
             "widgets": {},
-        },
-    }
+        }
+    )
+
+    if os.getenv("type") == "string":
+        module_name = "dropbase.worker.run_python_string"
+    else:
+        module_name = "dropbase.worker.run_python_file"
+
+    run_module = importlib.import_module(module_name)
 
     # Act
-    res = test_client.post("/query/python_string", json=data)
+    run_module.run(setup_redis, response)
+
+    time.sleep(1)
+
+    res = setup_redis.get(job_id)
+    res_data = json.loads(res)
 
     # Assert
-    assert res.status_code == 202
-    response_data = res.json()
-    job_id = response_data["job_id"]
+    assert res_data["status_code"] == 200
 
-    import time
-
-    time.sleep(2)
-
-    res = test_client.get(f"/query/status/{job_id}")
-    assert res.status_code == 200
-    res_data = res.json()
-    assert res_data["type"] == "table"
+    assert res_data.get("type") == "table"
 
     assert res_data["columns"][0]["name"] == "x"
     assert res_data["columns"][0]["column_type"] == "python"
