3d2cee46eb99da0008d07805bf056e640808b1a9
stephen-turtles <155136867+stephen-turtles@users.noreply.github.com>
2024-03-06 17:35:29 -0500

    Stephen/dba 817 add property tests to files (#151)
    * fixed update tests for components, tables, widgets (added time.sleep)
    
    * fixed all file tests
    
    * fixed sqlite db edit cell fail test
    
    * fixed app tests
    
    * added modifiying binded files table property tests

--- server/tests/routers/test_files.py
+++ server/tests/routers/test_files.py
@@ -2,6 +2,8 @@ import copy
 
 from server.tests.constants import TEST_APP_NAME, TEST_PAGE_NAME, WORKSPACE_PATH
 from server.tests.verify_file_exists import workspace_file_exists
+from server.tests.verify_property_exists import verify_property_exists
+from server.tests.verify_state_and_context import verify_object_in_state_context
 
 base_data = {
     "name": "REPLACE_ME",
@@ -262,3 +264,119 @@ def test_update_file_req(test_client):
     file_path = WORKSPACE_PATH.joinpath(f"{TEST_APP_NAME}/{TEST_PAGE_NAME}/scripts/{file_name}.sql")
     with open(file_path, "r") as r:
         assert r.read() == "mock sql"
+
+
+def test_delete_binded_fetcher(test_client):
+    # Arrange
+    if not workspace_file_exists("scripts/test_fetcher.sql"):
+        data = copy.deepcopy(base_data)
+        data["name"] = "test_fetcher"
+
+        test_client.post("/files/", json=data)
+
+        assert workspace_file_exists("scripts/test_fetcher.sql")
+
+    if not verify_object_in_state_context("TablesState", "table2"):
+        data = {
+            "app_name": TEST_APP_NAME,
+            "page_name": TEST_PAGE_NAME,
+            "properties": {
+                "tables": [
+                    {
+                        "label": "Table2",
+                        "name": "table2",
+                        "description": None,
+                        "fetcher": "test_fetcher",
+                        "height": "",
+                        "size": 10,
+                        "filters": None,
+                        "type": "sql",
+                        "smart": False,
+                        "columns": [],
+                        "depends_on": None,
+                    }
+                ],
+                "widgets": [],
+                "files": [{"name": "test_fetcher", "type": "sql", "source": None, "depends_on": []}],
+            },
+        }
+
+        headers = {"access-token": "mock access token"}
+
+        test_client.put("/page", json=data, headers=headers)
+
+    data = {
+        "app_name": TEST_APP_NAME,
+        "page_name": TEST_PAGE_NAME,
+        "file_name": "test_fetcher",
+        "type": "sql",
+    }
+
+    # Act
+    res = test_client.request("DELETE", "/files/", json=data)
+
+    # Assert
+    assert res.status_code == 200
+    assert not workspace_file_exists("scripts/test_fetcher.sql")
+
+    assert not verify_property_exists("tables[0].fetcher", "test_fetcher")
+    assert verify_property_exists("tables[0].fetcher", "")
+
+
+def test_rename_binded_fetcher(test_client):
+    # Arrange
+    if not workspace_file_exists("scripts/test_fetcher.sql"):
+        data = copy.deepcopy(base_data)
+        data["name"] = "test_fetcher"
+
+        test_client.post("/files/", json=data)
+
+        assert workspace_file_exists("scripts/test_fetcher.sql")
+
+    if not verify_object_in_state_context("TablesState", "table2"):
+        data = {
+            "app_name": TEST_APP_NAME,
+            "page_name": TEST_PAGE_NAME,
+            "properties": {
+                "tables": [
+                    {
+                        "label": "Table2",
+                        "name": "table2",
+                        "description": None,
+                        "fetcher": "test_fetcher",
+                        "height": "",
+                        "size": 10,
+                        "filters": None,
+                        "type": "sql",
+                        "smart": False,
+                        "columns": [],
+                        "depends_on": None,
+                    }
+                ],
+                "widgets": [],
+                "files": [{"name": "test_fetcher", "type": "sql", "source": None, "depends_on": []}],
+            },
+        }
+
+        headers = {"access-token": "mock access token"}
+
+        test_client.put("/page", json=data, headers=headers)
+
+    data = {
+        "app_name": TEST_APP_NAME,
+        "page_name": TEST_PAGE_NAME,
+        "old_name": "test_fetcher",
+        "new_name": "test_new_fetcher",
+        "type": "sql",
+    }
+
+    # Act
+    res = test_client.put("/files/rename", json=data)
+
+    # Assert
+    assert res.status_code == 200
+    assert not workspace_file_exists("scripts/test_fetcher.sql")
+    assert workspace_file_exists("scripts/test_new_fetcher.sql")
+
+    assert not verify_property_exists("tables[0].fetcher", "test_fetcher")
+    assert verify_property_exists("tables[0].fetcher", "test_new_fetcher")
