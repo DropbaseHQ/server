8088ae941b83ede9b69e500193281fc9d2cad6a3
stephen-turtles <stephen@dropbase.io>
2024-03-14 10:20:31 -0400

    parametrize edit cells

--- server/tests/routers/test_edit_cell.py
+++ server/tests/routers/test_edit_cell.py
@@ -1,6 +1,8 @@
+import copy
+
 import pytest
 
-data = {
+base_data = {
     # could be ignored, mocking in mock db
     "file": {"name": "demo_sql", "type": "sql", "source": "local", "depends_on": []},
     "edits": [
@@ -66,13 +68,44 @@ data = {
 }
 
 
-@pytest.mark.parametrize("mock_db", ["postgres", "mysql", "snowflake", "sqlite"], indirect=True)
-def test_edit_cell(test_client, mocker, mock_db):
+@pytest.fixture(scope="session")
+def get_edit_cell_data(request, test_client):
+    # Arrange
+    data = copy.deepcopy(base_data)
+
+    edit_values = request.param
+
+    data["edits"][0]["row"] = edit_values["row"]
+    data["edits"][0]["data_type"] = edit_values["data_type"]
+    data["edits"][0]["column_name"] = edit_values["column_name"]
+    data["edits"][0]["data_type"] = edit_values["data_type"]
+    data["edits"][0]["old_value"] = edit_values["old_value"]
+    data["edits"][0]["new_value"] = edit_values["new_value"]
+
+    return data
+
+
+@pytest.mark.parametrize("mock_db", ["postgres"], indirect=True)
+@pytest.mark.parametrize(
+    "get_edit_cell_data",
+    [
+        {
+            "row": {"user_id": 1, "username": "John Doe", "email": "john.doe@example.com"},
+            "column_name": "username",
+            "data_type": "VARCHAR(255)",
+            "column_type": "INTEGER",
+            "old_value": "John Doe",
+            "new_value": "Hello World",
+        }
+    ],
+    indirect=True,
+)
+def test_edit_cell(test_client, mocker, mock_db, get_edit_cell_data):
     # Arrange
     mocker.patch("server.controllers.edit_cell.connect", return_value=mock_db)
 
     # Act
-    res = test_client.post("/edit_cell/edit_sql_table/", json=data)
+    res = test_client.post("/edit_cell/edit_sql_table/", json=base_data)
     res_data = res.json()
 
     assert res.status_code == 200
@@ -85,8 +118,12 @@ def test_edit_cell_db_execute_fail(test_client, mocker, mock_db):
     mocker.patch("server.controllers.edit_cell.connect", return_value=mock_db)
 
     # Act
-    data["edits"][0]["row"] = {"user_id": 77, "username": "John Doe", "email": "john.doe@example.com"}
-    res = test_client.post("/edit_cell/edit_sql_table/", json=data)
+    base_data["edits"][0]["row"] = {
+        "user_id": 77,
+        "username": "John Doe",
+        "email": "john.doe@example.com",
+    }
+    res = test_client.post("/edit_cell/edit_sql_table/", json=base_data)
     res_data = res.json()
 
     # Assert
