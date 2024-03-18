73ac13ee7d6a320685bfd743fbdbb62eeb4e9189
stephen-turtles <stephen@dropbase.io>
2024-03-18 15:52:48 -0400

    adding stuff to docker

--- server/routers/tables.py
+++ server/routers/tables.py
@@ -1,32 +1,49 @@
-from fastapi import APIRouter, Depends, Response
+import json
+import uuid
 
-from dropbase.schemas.table import CommitTableColumnsRequest, ConvertTableRequest
+import anyio
+from fastapi import APIRouter, BackgroundTasks, Depends, Response
+
+from dropbase.schemas.table import CommitTableColumnsRequest, ConvertTableRequest, TableBase
 from server.auth.dependency import CheckUserPermissions
 from server.controllers.columns import commit_table_columns
+from server.controllers.python_docker import run_container
 from server.controllers.tables import convert_sql_table
 from server.requests.dropbase_router import DropbaseRouter, get_dropbase_router
 
-router = APIRouter(
-    prefix="/tables", tags=["tables"], responses={404: {"description": "Not found"}}
-)
+router = APIRouter(prefix="/tables", tags=["tables"], responses={404: {"description": "Not found"}})
+
+
+def convert_sql_table_sync_wrapper(req, router):
+    anyio.run(convert_sql_table, req, router)
 
 
 @router.post("/convert/")
-def convert_sql_table_req(
+async def convert_sql_table_req(
     req: ConvertTableRequest,
     response: Response,
+    background_tasks: BackgroundTasks,
     router: DropbaseRouter = Depends(get_dropbase_router),
 ):
-    resp, status_code = convert_sql_table(req, router)
-    response.status_code = status_code
-    return resp
+    job_id = uuid.uuid4().hex
+
+    args = {
+        "app_name": req.app_name,
+        "page_name": req.page_name,
+        "job_id": job_id,
+        "type": "smart",
+    }
+    args["table"] = json.dumps(req.table.dict())
+    args["state"] = json.dumps(req.state)
+
+    background_tasks.add_task(run_container, args)
+    # Now correctly scheduling the wrapper function
+    return {"message": "Conversion started", "job_id": job_id}
 
 
 @router.post(
     "/commit/",
-    dependencies=[
-        Depends(CheckUserPermissions(action="edit", resource=CheckUserPermissions.APP))
-    ],
+    dependencies=[Depends(CheckUserPermissions(action="edit", resource=CheckUserPermissions.APP))],
 )
 def commit_table_columns_req(req: CommitTableColumnsRequest, response: Response):
     try:
