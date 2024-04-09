ac3f8997c7915597a130567a8ca26a306dbd3d68
Jonathan <jon@dropbase.io>
2024-04-09 14:57:51 -0400

    Remove sync endpoints

--- server/requests/misc.py
+++ server/requests/misc.py
@@ -2,24 +2,5 @@ class MiscRouter:
     def __init__(self, session):
         self.session = session
 
-    def sync_table_columns(self, payload: dict):
-        return self.session.post(url="sync/columns/", json=payload)
-
-    def sync_components(self, app_name: str, page_name: str, token: str):
-        return self.session.post(
-            url="sync/components/",
-            json={
-                "app_name": app_name,
-                "page_name": page_name,
-                "token": token,
-            },
-        )
-
     def get_smart_columns(self, payload: dict):
         return self.session.post(url="get_smart_cols/", json=payload)
-
-    def sync_structure(self, payload: dict):
-        return self.session.post(url="sync/structure", json=payload)
-
-    def sync_app(self, payload: dict):
-        return self.session.post(url="sync/app", json=payload)
