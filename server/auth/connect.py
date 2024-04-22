f54ad71f7d3fcb8c5942393e635944ffb7a6338d
Jonathan <jon@dropbase.io>
2024-04-22 16:59:53 -0400

    Move db credentials to server credentials.py

--- server/auth/connect.py
+++ server/auth/connect.py
@@ -3,19 +3,14 @@ from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# from server.credentials import (
-#     POSTGRES_DB_HOST,
-#     POSTGRES_DB_NAME,
-#     POSTGRES_DB_PASS,
-#     POSTGRES_DB_PORT,
-#     POSTGRES_DB_USER,
-# )
+from server.credentials import (
+    POSTGRES_DB_HOST,
+    POSTGRES_DB_NAME,
+    POSTGRES_DB_PASS,
+    POSTGRES_DB_PORT,
+    POSTGRES_DB_USER,
+)
 
-POSTGRES_DB_HOST = "dropbase-dev.cvprjrvvsnqi.us-east-1.rds.amazonaws.com"
-POSTGRES_DB_NAME = "postgres"
-POSTGRES_DB_USER = "dropbase"
-POSTGRES_DB_PASS = "cXhw9DyumxDoIsh4PQ6z"
-POSTGRES_DB_PORT = "5432"
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_DB_USER}:{POSTGRES_DB_PASS}@{POSTGRES_DB_HOST}:{POSTGRES_DB_PORT}/{POSTGRES_DB_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
