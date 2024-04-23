1d0db976b8f66c22042f54908680b2eda177ddef
Jonathan <jon@dropbase.io>
2024-04-23 15:39:04 -0400

    Remove worker status

--- server/auth/crud/__init__.py
+++ server/auth/crud/__init__.py
@@ -6,7 +6,6 @@ from .token.crud_token import token
 from .user.crud_user import user
 from .user_group.crud_user_group import user_group
 from .user_role.crud_user_role import user_role
-from .worker_status.crud_worker_status import worker_status
 from .workspace.crud_workspace import workspace
 from .app.crud_app import app
 from .page.crud_page import page
--- server/auth/models/__init__.py
+++ server/auth/models/__init__.py
@@ -6,7 +6,6 @@ from .role import Role
 from .user import User
 from .user_group import UserGroup
 from .user_role import UserRole
-from .worker_status import WorkerStatus
 from .workspace import Workspace
 from .app import App
 from .page import Page
