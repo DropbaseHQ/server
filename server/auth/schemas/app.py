ddd865eaaf1b0c2f9430fd1eec756d0f0f3179f9
Jonathan <jon@dropbase.io>
2024-04-23 15:51:17 -0400

    Remove unused schemas

--- server/auth/schemas/app.py
+++ server/auth/schemas/app.py
@@ -1,12 +1,6 @@
 from pydantic import BaseModel
 
 
-class CreateAppRequest(BaseModel):
-    name: str
-    label: str
-    id: str
-
-
 class AppShareRequest(BaseModel):
     subjects: list[str]
     action: str
--- server/auth/schemas/page.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class CreatePageRequest(BaseModel):
-    name: str
-    label: str
-    id: str
-    app_id: str
