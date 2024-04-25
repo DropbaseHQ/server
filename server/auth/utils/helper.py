72c8b805669bdee213a59c185d4767fddcb008d1
Param Thakkar <param@dropbase.io>
2024-04-25 13:30:44 -0400

    Standardize error handling

--- server/auth/utils/helper.py
+++ server/auth/utils/helper.py
@@ -1,11 +1,5 @@
 from fastapi import HTTPException
 
 
-def create_error_details(message: str, error):
-    return {"message": message, "error": error.__str__()}
-
-
-# TODO: CHANGE TO STANDARD ERROR HANDLING
-def raise_http_exception(status_code: int, message: str, error=None):
-    error_detail = create_error_details(message, error)
-    raise HTTPException(status_code=status_code, detail=error_detail)
+def raise_http_exception(status_code: int, message: str):
+    raise HTTPException(status_code=status_code, detail=message)
