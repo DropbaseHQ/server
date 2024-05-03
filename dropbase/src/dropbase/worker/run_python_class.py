4fa03fa90a369e4a6b51d0d2caed73add3d4b999
Ayazhan Zhakhan <az@dropbase.io>
2024-05-03 09:53:38 -0700

    fixed edit cells

--- dropbase/src/dropbase/worker/run_python_class.py
+++ dropbase/src/dropbase/worker/run_python_class.py
@@ -41,10 +41,10 @@ def run(r, response):
             new_context = script.__getattribute__(resource).update(edits)
         elif action == "delete":
             row = state.get(resource).get("columns")
-            script.__getattribute__(resource).delete(row)
+            new_context = script.__getattribute__(resource).delete(row)
         elif action == "add":
             row = json.loads(os.getenv("row"))
-            script.__getattribute__(resource).add(row)
+            new_context = script.__getattribute__(resource).add(row)
         else:
             # action - on_select, on_click, on_input, on_tobble
             new_context = script.__getattribute__(resource).__getattribute__(
