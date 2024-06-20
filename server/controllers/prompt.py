6459b5fed003d2df52e5360ad362741a876ff059
Ayazhan Zhakhan <az@dropbase.io>
2024-06-20 19:55:07 -0400

    initial implementation of sonnet

--- server/controllers/prompt.py
+++ server/controllers/prompt.py
@@ -1,5 +1,6 @@
 import json
 
+import anthropic
 from openai import OpenAI
 
 from dropbase.schemas.prompt import Prompt
@@ -9,28 +10,57 @@ from server.helpers.prompt_composer import get_func_prompt, get_ui_prompt
 
 
 def handle_prompt(request: Prompt):
-    if server_envs.get("openai_api_key") is None:
-        raise Exception("OpenAI API key not found")
+    if not server_envs.get("llm"):
+        raise Exception("API key for an LLM model is missing")
 
+    # Get the prompt content
     base_path = f"workspace/{request.app_name}/{request.page_name}/"
     if request.type == "function":
         content = get_func_prompt(base_path, request.prompt)
     elif request.type == "ui":
         content = get_ui_prompt(base_path, request.prompt)
-
-    client = OpenAI(api_key=server_envs["openai_api_key"])
-    chat_completion = client.chat.completions.create(
-        messages=[{"role": "user", "content": content}],
-        model="gpt-4o",
-    )
-    updated_code = chat_completion.choices[0].message.content
+    # Invoke the model with the prompt
+    llmModel = LLMModel()
+    updated_code = llmModel.invoke(content)
+    # Remove markdown formatting from the updated code
     new_props = _remove_markdown_formatting(updated_code)
+
     if request.type == "function":
         return new_props
     if request.type == "ui":
         pageController = PageController(request.app_name, request.page_name)
         pageController.update_page_properties(json.loads(new_props))
         return {"message": "success"}
+
+
+class LLMModel:
+    def __init__(self):
+
+        default_models = {"openai": "gpt-4o", "anthropic": "claude-3-5-sonnet-20240620"}
+        llms = server_envs.get("llm")
+        self.provider_name = next(iter(llms))
+        self.provider = llms.get(self.provider_name)
+        self.api_key = self.provider.get("api_key")
+        self.model = self.provider.get("model") or default_models.get(self.provider_name)
+        self.max_tokens = self.provider.get("max_tokens") or 4096
+
+    def invoke(self, content):
+        method = getattr(self, self.provider_name)
+        return method(content)
+
+    def openai(self, content: str) -> str:
+        client = OpenAI(api_key=self.api_key)
+        messages = [{"role": "user", "content": content}]
+        message = client.chat.completions.create(
+            model=self.model, max_tokens=self.max_tokens, messages=messages
+        )
+        return message.choices[0].message.content
+
+    def anthropic(self, content: str) -> str:
+        client = anthropic.Anthropic(api_key=self.api_key)
+        messages = [{"role": "user", "content": content}]
+        message = client.messages.create(model=self.model, max_tokens=self.max_tokens, messages=messages)
+        return message.content[0].text
 
 
 def _remove_markdown_formatting(code_snippet):
