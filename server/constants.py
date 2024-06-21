65143767d870a07ddd11f514bc2d11ebfcaa7e4a
Ayazhan Zhakhan <46544198+azhakhan@users.noreply.github.com>
2024-06-21 14:27:36 -0700

    Az/sonnet (#270)
    * initial implementation of sonnet
    
    * moved model name to config
    
    * added provider and model to request, added defaults for llm
    
    * added support for anthropic

--- server/constants.py
+++ server/constants.py
@@ -16,3 +16,18 @@ DEFAULT_RESPONSES = {404: {"description": "Not found"}}
 INFER_TYPE_SAMPLE_SIZE = 50
 WORKER_VERSION = "0.4.*"
 ONBOARDING_URL = "https://onboarding.dropbase.io"
+
+# configure default llm provider and model
+LLMS = server_envs.get("llm")
+if LLMS:
+    # get the first provider
+    DEFAULT_PROVIDER = next(iter(LLMS))
+    default_provider_models = {"openai": "gpt-4o", "anthropic": "claude-3-5-sonnet-20240620"}
+    DEFAULT_MODEL = LLMS.get(DEFAULT_PROVIDER).get("model") or default_provider_models.get(
+        DEFAULT_PROVIDER
+    )
+else:
+    DEFAULT_PROVIDER = None
+    DEFAULT_MODEL = None
+
+DEFAULT_MAX_TOKENS = 4096
--- server/controllers/prompt.py
+++ server/controllers/prompt.py
@@ -1,36 +1,73 @@
 import json
 
+from anthropic import Anthropic
 from openai import OpenAI
 
 from dropbase.schemas.prompt import Prompt
 from server.config import server_envs
+from server.constants import DEFAULT_MAX_TOKENS, DEFAULT_MODEL, DEFAULT_PROVIDER
 from server.controllers.page_controller import PageController
 from server.helpers.prompt_composer import get_func_prompt, get_ui_prompt
 
 
 def handle_prompt(request: Prompt):
-    if server_envs.get("openai_api_key") is None:
-        raise Exception("OpenAI API key not found")
+    if not server_envs.get("llm"):
+        raise Exception("API key for an LLM model is missing")
 
+    # Get the prompt content
     base_path = f"workspace/{request.app_name}/{request.page_name}/"
     if request.type == "function":
         content = get_func_prompt(base_path, request.prompt)
     elif request.type == "ui":
         content = get_ui_prompt(base_path, request.prompt)
 
-    client = OpenAI(api_key=server_envs["openai_api_key"])
-    chat_completion = client.chat.completions.create(
-        messages=[{"role": "user", "content": content}],
-        model="gpt-4o",
-    )
-    updated_code = chat_completion.choices[0].message.content
+    # Get the provider and model from the request or use the default
+    provider = request.provider or DEFAULT_PROVIDER
+    model = request.model or DEFAULT_MODEL
+
+    # Invoke the model with the prompt
+    llmModel = LLMModel(provider, model)
+    updated_code = llmModel.invoke(content)
+    # Remove markdown formatting from the updated code
     new_props = _remove_markdown_formatting(updated_code)
+
     if request.type == "function":
         return new_props
     if request.type == "ui":
         pageController = PageController(request.app_name, request.page_name)
         pageController.update_page_properties(json.loads(new_props))
         return {"message": "success"}
+
+
+class LLMModel:
+    def __init__(self, provider: str, model: str):
+        llms = server_envs.get("llm")
+        self.provider = provider
+        self.model = model
+
+        # api_key and config are required for the provider
+        self.api_key = llms.get(provider).get("api_key")
+        self.config = llms.get(provider).get("config") or {}
+
+    def invoke(self, content):
+        # call the provider's method by name
+        method = getattr(self, self.provider)
+        return method(content)
+
+    def openai(self, content: str) -> str:
+        client = OpenAI(api_key=self.api_key)
+        messages = [{"role": "user", "content": content}]
+        message = client.chat.completions.create(model=self.model, messages=messages, **self.config)
+        return message.choices[0].message.content
+
+    def anthropic(self, content: str) -> str:
+        client = Anthropic(api_key=self.api_key)
+        messages = [{"role": "user", "content": content}]
+        if self.config == {}:
+            # anthropic requires a max_tokens parameter
+            self.config = {"max_tokens": DEFAULT_MAX_TOKENS}
+        message = client.messages.create(model=self.model, messages=messages, **self.config)
+        return message.content[0].text
 
 
 def _remove_markdown_formatting(code_snippet):
